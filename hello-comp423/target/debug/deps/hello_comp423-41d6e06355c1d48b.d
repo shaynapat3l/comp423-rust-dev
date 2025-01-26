ead6d0d4b4e81a92ae8c57fb9489e5eec1e6c53b
Shayna Patel <shaynap@unc.edu>
2025-01-26 20:22:09 +0000

    Rust tutorial with Dev Container - giving desired output

---
+++ hello-comp423/target/debug/deps/hello_comp423-41d6e06355c1d48b.d
@@ -0,0 +1,5 @@
+/workspaces/comp423-rust-dev/hello-comp423/target/debug/deps/hello_comp423-41d6e06355c1d48b: src/main.rs
+
+/workspaces/comp423-rust-dev/hello-comp423/target/debug/deps/hello_comp423-41d6e06355c1d48b.d: src/main.rs
+
+src/main.rs:
---
+++ hello-comp423/target/debug/hello-comp423.d
@@ -0,0 +1,1 @@
+/workspaces/comp423-rust-dev/hello-comp423/target/debug/hello-comp423: /workspaces/comp423-rust-dev/hello-comp423/src/main.rs
